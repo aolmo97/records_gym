0311491e96925f36756f29ade8e1490e3aa1d06a
Antonio <aolmolopez97@gmail.com>
2023-11-19 15:57:37 +0100

    feat: add pwa and logos

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,60 @@
-import { defineConfig } from 'vite';
-
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+const baseUrl ="/records_gym/";
 export default defineConfig({
-    base: '/records_gym/'
+  base: `${baseUrl}`,
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "Record Gym App",
+        short_name: "RecordGymApp",
+        theme_color: "#522633",
+        icons: [
+          {
+            src: `${baseUrl}assets/images/logos/android-chrome-36x36.png`,
+            sizes: "36x36",
+            type: "image/png",
+          },
+          {
+            src: `${baseUrl}assets/images/logos/android-chrome-48x48.png`,
+            sizes: "48x48",
+            type: "image/png",
+          },
+          {
+            src: `${baseUrl}assets/images/logos/android-chrome-72x72.png`,
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: `${baseUrl}assets/images/logos/android-chrome-96x96.png`,
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: `${baseUrl}assets/images/logos/android-chrome-144x144.png`,
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: `${baseUrl}assets/images/logos/android-chrome-192x192.png`,
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: `${baseUrl}assets/images/logos/android-chrome-256x256.png`,
+            sizes: "256x256",
+            type: "image/png",
+            purpose: "any maskable"
+          },
+          {
+            src: `${baseUrl}assets/images/logos/android-chrome-512x512.png`,
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
-  
